5f0346eb50944fc7a545394bffb81b32eb1d68b0
Chris Heinrich <cpheinrich@gmail.com>
2019-10-22 07:44:23 -0700

    more options for create_tour script

--- earth_tour/create_tour.py
+++ earth_tour/create_tour.py
@@ -1,13 +1,16 @@
 from earth_tour.tour_generator import TourGenerator
 import argparse
+import time
 
 
 def main(args):
-    tg = TourGenerator(input_path=args.input_csv, output_path=args.output_path)
-    tg.create_tour()
+    tg = TourGenerator(input_path=args.input_csv, output_path=args.output_path, max_rows=args.max_rows)
+    if args.create:
+        tg.create_tour()
     if args.capture:
         tg.open_tour()
-        tg.capture_tour()
+        time.sleep(6)
+        tg.capture_tour(args)
 
 
 if __name__ == '__main__':
@@ -16,7 +19,13 @@ if __name__ == '__main__':
                         help="The path to the input csv file")
     parser.add_argument('--output_path', default=None, help="""Path to .kml file to output. If not supplied,
      it will use the input_csv path using the .kml extension""")
-    parser.add_argument('--capture', action='store_false',
+    parser.add_argument('--create', action='store_true',
                         help="""Whether or not to capture the tour""")
+    parser.add_argument('--capture', action='store_true',
+                        help="""Whether or not to capture the tour""")
+    parser.add_argument('--max_rows', type=int,
+                        help="""Max number of rows. If not set defaults to all rows""")
+    parser.add_argument('--no_reroof', action='store_true',
+                        help="Will write that a reroof did not occur in the metadata if passed")
     args = parser.parse_args()
     main(args)
--- earth_tour/tour_generator.py
+++ earth_tour/tour_generator.py
@@ -15,7 +15,7 @@ The input csv is expected to contain columns labeled 'latitude' and 'longitude'.
 
 
 class TourGenerator(object):
-    def __init__(self, input_path, output_path=None):
+    def __init__(self, input_path, output_path=None, max_rows=None):
         self.input_path = input_path
         if output_path:
             self.output_path = output_path
@@ -31,6 +31,10 @@ class TourGenerator(object):
         self.cycle_time = self.fly_time + self.wait_time
         self.altitude = 50  # Altitude of virtual camera in meters
         self.address_col = 'site_address'  # Name of column with address
+        if max_rows:
+            self.max_rows = max_rows
+        else:
+            self.max_rows = len(self.data)
 
     def create_fly_to(self, kmlDoc, row, size_range=100, year='2010'):
         """ Creates a FlyTo element for the tour """
@@ -74,7 +78,8 @@ class TourGenerator(object):
         return wait
 
     def create_tour(self):
-      """ Creates the google earth tour and writes to .kml """
+        """Creating tour """ 
+        print('Creating tour')
         kmlDoc = xml.dom.minidom.Document()
         kmlElement = kmlDoc.createElementNS(
             'http://earth.google.com/kml/2.2', 'kml')
@@ -97,8 +102,8 @@ class TourGenerator(object):
         tourElement.appendChild(name)
 
         playlistElement = kmlDoc.createElement('gx:Playlist')
-        # Add each row in csv as a flyto element
-        for index, row in self.data.iterrows():
+        # Add each row in csv as a flyto elementip
+        for index, row in self.data.iloc[:self.max_rows].iterrows():
             for year in self.years:
                 placemarkElement = self.create_fly_to(kmlDoc, row, year=year)
                 playlistElement.appendChild(placemarkElement)
@@ -110,11 +115,12 @@ class TourGenerator(object):
         kmlFile = open(self.output_path, 'wb')
         kmlFile.write(kmlDoc.toprettyxml('  ', newl='\n', encoding='utf-8'))
 
+
     def open_tour(self):
         """ Opens the google earth tour file """
         subprocess.call(['/usr/bin/open', self.output_path])
 
-    def write_metadata(self, row, path):
+    def write_metadata(self, row, path, args):
         """ Writes the metadata associated with a property to json. Requires this metadata to be in the .csv, otherwise
         this step is skipped. """
         try:
@@ -122,15 +128,19 @@ class TourGenerator(object):
             metadata['address'] = row['site_address']
             metadata['latitude'] = row['latitude']
             metadata['longitude'] = row['longitude']
-            metadata['reroof_permit_issue_date'] = row['reroof_permit_issue_date']
-            metadata['reroof_permit_expiration_date'] = row['reroof_permit_expiration_date']
-            metadata['reroof_type'] = row['reroof_type']
+            if args.no_reroof:
+                metadata['reroof_type'] = 'no_reroof'
+            else:
+                metadata['reroof_permit_issue_date'] = row['reroof_permit_issue_date']
+                metadata['reroof_permit_expiration_date'] = row['reroof_permit_expiration_date']
+                metadata['reroof_type'] = row['reroof_type']
+                
             with open(path, 'w+') as f:
                 json.dump(metadata, f)
         except Exception as e:
             print('Failed to write metadata with error {}'.format(e))
 
-    def capture_tour(self):
+    def capture_tour(self, args):
         output_dir = os.path.join(os.path.dirname(
             self.output_path), 'tour_images')
         os.makedirs(output_dir, exist_ok=True)
@@ -143,7 +153,7 @@ class TourGenerator(object):
                 output_dir, row[self.address_col].replace(' ', '_'))
             metadata_path = os.path.join(prop_dir, 'metadata.json')
             os.makedirs(prop_dir, exist_ok=True)
-            self.write_metadata(row, metadata_path)
+            self.write_metadata(row, metadata_path, args)
             for year in self.years:
                 try:
                     img_path = os.path.join(prop_dir, '{}.png'.format(year))
